a120933fe803d5fdd0d8c20452e021b4ab3510af
Amadi Jerry <amadijerry310@gmail.com>
2024-06-09 19:19:57 +0100

    add login and logout endpoints

--- internal/user/controller.go
+++ internal/user/controller.go
@@ -25,8 +25,36 @@ func (c *Controller) CreateUser(ctx *gin.Context) {
 
 	user, err := c.Service.CreateUser(ctx.Request.Context(), &u)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 	ctx.JSON(http.StatusCreated, *user)
+}
+
+func (c *Controller) LoginUser(ctx *gin.Context) {
+	var user LoginUserReq
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	u, err := c.Service.Login(ctx.Request.Context(), &user)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	ctx.SetCookie("jwt", u.AccessToken, 3600, "/", "localhost", false, true)
+
+	res := &LoginUserRes{
+		Username: u.Username,
+		ID:       u.ID,
+	}
+
+	ctx.JSON(http.StatusOK, *res)
+
+}
+
+func (c *Controller) Logout(ctx *gin.Context) {
+	ctx.SetCookie("jwt", "", -1, "", "", false, true)
+	ctx.JSON(http.StatusOK, gin.H{"message": "logout successful"})
 }
--- router/router.go
+++ router/router.go
@@ -10,6 +10,8 @@ var r *gin.Engine
 func InitRouter(userController *user.Controller){
 	r = gin.Default()
 	r.POST("/signup", userController.CreateUser)
+	r.POST("/login", userController.LoginUser)
+	r.GET("/logout", userController.Logout)
 }
 
 func Start(addr string) error{
